1abf0adcd67696cee182b3e5e3d0a77a4fb1837c
Aibek Urazov PC <ibek-94@mail.ru>
2025-11-07 17:33:28 +0500

    saucedemo 2

--- sausedemo.py
+++ sausedemo.py
@@ -1,12 +1,13 @@
 from playwright.sync_api import Playwright, sync_playwright, expect
+import time
 
-def sausedemo(playwright: Playwright) -> None:
+def sausedemo_buy_something(playwright: Playwright) -> None:
     browser = playwright.chromium.launch(headless=False, slow_mo=900)
     context = browser.new_context()
     page = context.new_page()
     
     page.goto("https://www.saucedemo.com/")
-    page.locator("#user-name").fill("problem_use2r")  # лучше использовать fill вместо press_sequentially
+    page.locator("#user-name").fill("problem_user")  # лучше использовать fill вместо press_sequentially
     page.locator("#password").fill("secret_sauce")
     page.locator("#login-button").click()  # добавлены скобки!
 
@@ -16,9 +17,29 @@ def sausedemo(playwright: Playwright) -> None:
     else:
         print("Успешный вход!")
 
+    #Собираем все заказы в список и преобразуем сразу в строку с маленькой буквы и заменяем пробелы -
+    items_name = [item.replace(' ', '-').lower() for item in page.locator(".inventory_item_name").all_text_contents()]
+
+    print(f"Всего заказов: {len(items_name)}")
+    print(f"Последний заказ на странице: {items_name[-1]}")
+    print(f"Список товаров: {items_name}")
+
+    #Добавляем все заказы в корзину
+    for item in range(len(items_name)-1):
+        page.locator(f"#add-to-cart-{items_name[item]}").click()
+        print (page.locator(f"#add-to-cart-{items_name[item]}"))
+    
+    page.locator(".shopping_cart_link").click()
+
+    time.sleep(10)
+
+
+
+    
+
     context.close()
     browser.close()
 
 if __name__ == "__main__":
     with sync_playwright() as playwright:
-        sausedemo(playwright)
+        sausedemo_buy_something(playwright)
